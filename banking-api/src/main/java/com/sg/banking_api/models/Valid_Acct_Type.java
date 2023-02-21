7159a4763db4646e510030aaddd88dce2121bbdd
Danielle James <dosbor3@gmail.com>
2023-02-21 14:27:44 -0600

    add members to Valid_Acct_Type model

--- banking-api/src/main/java/com/sg/banking_api/models/Valid_Acct_Type.java
+++ banking-api/src/main/java/com/sg/banking_api/models/Valid_Acct_Type.java
@@ -1,4 +1,22 @@
 package com.sg.banking_api.models;
 
 public class Valid_Acct_Type {
+    private String account_type;
+    private String account_description;
+
+    public String getAccount_type() {
+        return account_type;
+    }
+
+    public void setAccount_type(String account_type) {
+        this.account_type = account_type;
+    }
+
+    public String getAccount_description() {
+        return account_description;
+    }
+
+    public void setAccount_description(String account_description) {
+        this.account_description = account_description;
+    }
 }
