e53378f88bd367b69cb70c4ed1cbcda2eb95c6fb
Danielle James <dosbor3@gmail.com>
2023-02-21 13:25:34 -0600

    add members to checking model

--- banking-api/src/main/java/com/sg/banking_api/models/Checking.java
+++ banking-api/src/main/java/com/sg/banking_api/models/Checking.java
@@ -1,4 +1,70 @@
 package com.sg.banking_api.models;
 
+import java.math.BigDecimal;
+
 public class Checking {
+    private String account_number;
+    private String customer_number;
+    private BigDecimal deposits;
+    private BigDecimal withdrawls;
+    private BigDecimal current_balance;
+    private BigDecimal available_balance;
+    private String account_type;
+
+    public String getAccount_number() {
+        return account_number;
+    }
+
+    public void setAccount_number(String account_number) {
+        this.account_number = account_number;
+    }
+
+    public String getCustomer_number() {
+        return customer_number;
+    }
+
+    public void setCustomer_number(String customer_number) {
+        this.customer_number = customer_number;
+    }
+
+    public BigDecimal getDeposits() {
+        return deposits;
+    }
+
+    public void setDeposits(BigDecimal deposits) {
+        this.deposits = deposits;
+    }
+
+    public BigDecimal getWithdrawls() {
+        return withdrawls;
+    }
+
+    public void setWithdrawls(BigDecimal withdrawls) {
+        this.withdrawls = withdrawls;
+    }
+
+    public BigDecimal getCurrent_balance() {
+        return current_balance;
+    }
+
+    public void setCurrent_balance(BigDecimal current_balance) {
+        this.current_balance = current_balance;
+    }
+
+    public BigDecimal getAvailable_balance() {
+        return available_balance;
+    }
+
+    public void setAvailable_balance(BigDecimal available_balance) {
+        this.available_balance = available_balance;
+    }
+
+    public String getAccount_type() {
+        return account_type;
+    }
+
+    public void setAccount_type(String account_type) {
+        this.account_type = account_type;
+    }
+
 }
